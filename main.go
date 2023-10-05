832ba28050faf244464d7dc429991a7d7f392ee1
100xlong <long@matricalabs.io>
2023-10-05 18:19:42 -0400

    gin basic

--- main.go
+++ main.go
@@ -1,16 +1,58 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
-    http.HandleFunc("/", Hello)
-    fmt.Println("Server is running on port 8080...")
-    http.ListenAndServe(":8080", nil)
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
 }
 
-func Hello(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, Golang!")
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func getAlbumById(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, album := range albums {
+		if album.ID == id {
+			c.IndentedJSON(http.StatusOK, album)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumById)
+	router.POST("/albums", postAlbums)
+
+	router.Run("localhost:8080")
 }
